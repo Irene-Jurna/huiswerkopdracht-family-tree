30d1c9eca60ad19f38b2cd760af2c1e4d201cf0f
Irene <irene.jurna@gmail.com>
2025-04-14 11:19:34 +0200

    Constructors, getters, setters, methods Person klasse toegevoegd

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -14,4 +14,105 @@ public class Person {
     List<Person> siblings;
     List<Person> children;
     List<Pet> pets;
+
+    public Person(String name, String lastName, int age, String sex) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public Person(String name, String middleName, String lastName, String sex, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+
+    public List<Person> getSiblings() {
+        return siblings;
+    }
+
+    public void setSiblings(List<Person> siblings) {
+        this.siblings = siblings;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    void addParents() {};
+    void addChildren() {};
+    void addPets() {};
+    void addSiblings() {};
+    void getGrandChildren() {};
 }
