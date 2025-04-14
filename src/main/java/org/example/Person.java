a50fe6383b823b80dcae8ed3adf58bd0f5c83205
Irene <irene.jurna@gmail.com>
2025-04-14 11:17:00 +0200

    Variabelen toegevoegd

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+    String name;
+    String middleName;
+    String lastName;
+    String sex;
+    int age;
+    Person mother;
+    Person father;
+    List<Person> siblings;
+    List<Person> children;
+    List<Pet> pets;
 }
--- src/main/java/org/example/Pet.java
+++ src/main/java/org/example/Pet.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Pet {
+    String name;
+    int age;
+    String species;
+    Person owner;
 }
