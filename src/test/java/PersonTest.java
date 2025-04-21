54e11bc233a0e15caa32f5d5e79abe5f3b7901b0
Irene <irene.jurna@gmail.com>
2025-04-21 07:08:48 +0200

    Junit toegevoegd en eerste getter/setter getest

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,2 +1,29 @@
+import org.example.Person;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PersonTest {
+
+    Person testPerson;
+
+    @BeforeEach
+    void setUp() {
+        testPerson = new Person("Anne", "Maria", "Smid", "vrouw", 40);
+    }
+
+    @Test
+    public void testGetName() {
+        String name = testPerson.getName();
+
+        assertEquals("Anne", name);
+    }
+
+    @Test
+    public void testSetName() {
+        testPerson.setName("Loes");
+
+        assertEquals("Loes", testPerson.getName());
+    }
 }
